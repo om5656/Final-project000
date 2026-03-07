14f4dbbb3cad3f32712756f637dabf6a38322ccd
om5656 <mohamed56562026@hotmail.com>
2026-03-07 16:17:00 +0200

    Add Register component for user sign up

---
+++ Register.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
+        <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} required />
+        <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} required />
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
